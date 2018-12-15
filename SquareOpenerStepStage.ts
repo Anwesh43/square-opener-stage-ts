7aedf854d4e539981a08f13446b94a836581bec8
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 21:51:39 +0530

    adding drawSOSNode function

--- SquareOpenerStepStage.ts
+++ SquareOpenerStepStage.ts
@@ -18,6 +18,34 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
     return (1 - k) / a + k / b
 }
 
+const drawSOSNode : Function = (context, i, scale) => {
+    const gap : number = w / (nodes + 1)
+    const size : number = gap / sizeFactor
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    context.strokeStyle = color
+    context.lineCap = 'round'
+    context.lineWidth = Math.min(w, h) / strokeFactor 
+    context.save()
+    context.translate(gap * (i + 1), h / 2)
+    for (var j = 0; j < lines; j++) {
+        const scc1 : number = divideScale(sc1, j, lines)
+        const scc2 : number = divideScale(sc2, j, lines)
+        context.save()
+        context.rotate(Math.PI/2 * j)
+        context.save()
+        context.translate(-size, size)
+        context.rotate(Math.PI/2 * scc2)
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(-2 * size * scc1, 0)
+        context.stroke()
+        context.restore()
+        context.restore()
+    }
+    context.restore()
+}
+
 const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * scGap * dir
 }
