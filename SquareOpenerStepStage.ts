8d76d5218718f5527e6d3b7063311ff21d6f6abf
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 22:08:38 +0530

    adding SquareOpenerStep class

--- SquareOpenerStepStage.ts
+++ SquareOpenerStepStage.ts
@@ -166,3 +166,25 @@ class SOSNode {
         return this
     }
 }
+
+class SquareOpenerStep {
+    curr : SOSNode = new SOSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
