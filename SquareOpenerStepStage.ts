0cbe5dcc0c33dfe0ffedd6cd0f8296ec8f347c14
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 21:41:39 +0530

    adding scale functions

--- SquareOpenerStepStage.ts
+++ SquareOpenerStepStage.ts
@@ -1,5 +1,27 @@
 const w : number = window.innerWidth, h : number = window.innerHeight
 const nodes : number = 5
+const lines : number = 4
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const sizeFactor : number = 3
+const strokeFactor : number = 90
+const color : string = "#e74c3c"
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, Math.max(0, scale - i / n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * scGap * dir
+}
+
 class SquareOpenerStepStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
